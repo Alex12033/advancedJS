e8632a57e9b040640690e383b0995eda4edbf8a2
Aleksandr <aleksandrignashov42@gmail.com>
2021-11-21 13:46:26 +0200

    new lern work with date in js

--- scriptJs/script.js
+++ scriptJs/script.js
@@ -1,5 +1,5 @@
 
-const btn = document.querySelector('.btn');
+/* const btn = document.querySelector('.btn');
 let timerId,
     i = 0;
 
@@ -23,16 +23,43 @@ btn.addEventListener('click', myAnimation);
 
 
 
-//function logger() {
-//    if (i == 3) {
-//        clearInterval(timerId);
-//    }
-//    console.log('heloooo');
-//    i++;
-//}
-//
-//let id = setTimeout(function log(){
-//    console.log('hello');
-//    id = setTimeout(log, 500);
-//}, 500);
+function logger() {
+   if (i == 3) {
+       clearInterval(timerId);
+   }
+   console.log('heloooo');
+   i++;
+}
 
+let id = setTimeout(function log(){
+   console.log('hello');
+   id = setTimeout(log, 500);
+}, 500);
+ */
+
+/* const now = new Date('2021-11-22');
+new Date.parse('2021-11-22');
+
+console.log(now.setHours(40));
+console.log(now);
+
+
+
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getUTCHours());
+
+console.log(now.getTimezoneOffset());
+console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 1000000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Cickle work ${end - start} milisecond`); */

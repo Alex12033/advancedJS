cb861fc83d8ee261e5c1243970118278e113c732
Aleksandr <aleksandrignashov42@gmail.com>
2021-11-23 21:45:53 +0200

    classes

--- scriptJs/script.js
+++ scriptJs/script.js
@@ -171,4 +171,29 @@ obj.sayNamber(); */
 //console.log(calc(2));
 
 
- 
+/* class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+    
+    showMyProps() {
+        console.log(`Text: ${this.text}, color ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+ div.showMyProps();
+ console.log(div.calcArea(20, 20)); */
